195400e09429393b201f26fbc2b502cf3294b710
Reggie188 <mr.tulyakov92@gmail.com>
2023-04-07 21:45:18 +0300

    update 1 task'

--- pyLess_8/createDirFile.py
+++ pyLess_8/createDirFile.py
@@ -6,7 +6,6 @@
 
 import os
 
-
 def checkInput(message: str) -> int:
     checkNum = input(message)
     if checkNum.replace("-", '').isdigit():
@@ -24,17 +23,18 @@ def ensure_dir(dirname):
 def createFile(nameFile, dirName):
     return open(os.path.join(os.path.dirname(__file__), dirName, f'{nameFile}.txt'), 'w')
 
-def writeInF(nameFile):
+'''def writeInF(nameFile):
     with open(nameFile, 'a+', encoding='UTF-8') as m_file:
         m_file.write(f'{input("Enter: ")}\n')
-    return m_file
+    return m_file'''
 
 def showDir(dirname) -> None:
+    print('Files: ')
     for i, val in enumerate(os.listdir(dirname)):
-        print(f"{i + 1}: {val}")
+        print(f"{i + 1} - {val}")
 
-def openDir():
-    return os.listdir('directory')
+'''def openDir():
+    return os.listdir('LibDir')'''
 
 def openFile(nameFile):
     with open(nameFile, 'r', encoding='UTF-8') as m_file:
@@ -43,13 +43,45 @@ def openFile(nameFile):
 
 def choiseCommand(flag):
     comandMain = ["Open libraris", "Stop program"]
-    comandLibr = ["Choise file", "Exit to main"]
-    comandFile = ["Update file", 'Create new file', 'Delete file', "Exit to main"]
+    comandLibr = ['Create new file', "Choise file", "Exit to main"]
+    comandFile = ["Update file", 'Delete file', "Exit to main"]
     lstCmd = [comandMain, comandLibr, comandFile]
+    print('\nCommands: ')
     for i, val in enumerate(lstCmd[flag]):
         print(f"{i + 1}: {val}")
 
-def main(flag):
+def menu(exit):
+    if exit == True:
+        main(False)
+    ensure_dir('LibDir')
+    choiseCommand(0)
+    userAnswMain = checkInput('\nChoise command:')
+    if userAnswMain == 1:
+        showDir('LibDir'), choiseCommand(1)
+        userAnswLibr = checkInput('\nChoise command:')
+        if userAnswLibr == 1:
+            createFile(input('Enter file name: '), 'LibDir')
+            showDir('LibDir'), choiseCommand(1)
+            choiseCommand(2)
+            userAnswFails = checkInput('\nChoise command:')
+            openFile(f'LibDir/{os.listdir("LibDir")[checkInput("Choise file: ") - 1]}')
+        else:
+            menu(False)
+
+    else: main(False)
+
+
+def main(q):
+    if q == False:
+        print('Ok, Bye!')
+        return
+    print(f'Start program...')
+    menu(False)
+
+if __name__ == '__main__':
+    main(True)
+
+'''def main(flag):
     if flag == False:
         return
     while True:
@@ -70,23 +102,12 @@ def main(flag):
                 choiseCommand(2)
                 userAnswFile = checkInput('\nChoise command: ')
                 if userAnswFile == 1:
-                    writeInF(pathFile)
+                    writeInF(pathFile)'''
 
+'''print(f'Use dir - {os.getcwd()}')
+#os.mkdir('directory')
+os.chdir('directory')
+print(f'Use dir>>> - {os.getcwd()}')
+os.chdir('..')
+print(f'Use dir<<< - {os.getcwd()}')'''
 
-if __name__ == '__main__':
-    main(True)
-
-
-'''answ_user = input("Enter y if you want create file and write anything in file: ")
-if answ_user.lower() == 'y':
-    filename = input('Enter name file: ')
-    userFile = createFile(filename)
-    while True:
-        user_enter = input('Enter surname and prem: ')
-        if not user_enter:
-            break
-        else:
-            writeInF(file_user, user_enter)'''
-
-'''createDir('testDir')
-createFile('test', 'testDir')'''
